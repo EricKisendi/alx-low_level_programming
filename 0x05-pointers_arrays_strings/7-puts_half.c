4c9302e95bfb2ae4691e59e6ad95b41df4d82ff3
EricKisendi <erickisendi18@gmail.com>
2022-12-21 18:46:50 +0300

    the better half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * puts_half - prints half of a string followed by a new line'
+  *
+  * @str: input string
+  *
+  * Return: void
+  */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
