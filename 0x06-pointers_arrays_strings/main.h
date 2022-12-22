5e60d21eca0cd42216a3b05b02168d1bb64303fb
EricKisendi <erickisendi18@gmail.com>
2022-12-22 13:26:07 +0300

    concatination of strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -9,6 +6,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-int _putchar(char *c)
-
-#endif
+int _putchar(char);
