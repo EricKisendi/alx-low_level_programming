3ff931a643f5ad9210a3aa5f483d7024da565fa8
Erick Kisendi <erickisendi18@gmail.com>
2023-02-07 00:46:24 +0300

    checks endianess

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *get_endianness - checks endianess
+ *Return:0- big endian, 1- small endian
+ */
+int get_endianness(void)
+{
+	int i = 1;
+	char *end = (char *)&i;
+
+	if (*end)
+		return (1);
+	return (0);
+}
